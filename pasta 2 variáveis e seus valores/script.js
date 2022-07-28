20a255e1652670e6302fbedeb11c4abcb0000f00
alinemello29 <109696840+alinemello29@users.noreply.github.com>
2022-07-28 18:50:55 -0300

    2

---
+++ pasta 2 variáveis e seus valores/script.js
@@ -0,0 +1,117 @@
+/*  //tipos primitivos
+  // boolean 
+  var boolean = false; 
+  console.log( ' A variável ${boolean} tem o tipo ${typeof(boolean)}'); 
+
+  // number 
+  var numero= 1; 
+  console.log('A variável ${numero} tem o tipo ${typeof(função)}'); 
+
+  //string 
+  var nome = 'aline'; 
+  console.log('A variável ${nome} tem tipo $ {typeof(nome)}'); 
+    
+  //function 
+  var função = function(){}; 
+  console.log('A variável ${função} tem o tipo{typeof(função)}');
+
+
+  //como declarar 
+
+  var;//variável global ou local - valor inicial nulo 
+
+  let;//variável local de bloco - valor inicial nulo 
+
+  const;//variável local de bloco - valor inicial obrigatório */ 
+
+
+  //usando var global 
+  /*var nome = 'aline'; 
+  var nome = 'Aline'; 
+
+  // usando var local 
+  function nomeDafuncao() {
+    var sobrenome = 'melo'; 
+    console.log(sobrenome);
+  }
+
+  console.log(nome);
+  nomeDafuncao(); */ 
+
+  // as propiedades de var se aplicam a let, mas por conversão usamos let no escopo local
+
+
+  /* //usando const 
+  const constante = 'constante';
+  //valor não pode ser alterado; */ 
+
+
+
+  /* // atribuição 
+  var atribuição =' aline'; 
+   
+  //comparação 
+  var caomparação = '0' == 0; 
+  console.log(comparacao);
+
+  //comparação identica 
+  var comparacaoIdentica = "0" == 0; 
+  console.log(comparacaoIdentica); */
+
+  
+  /* //opernado aritméticos 
+  //adição 
+  var adicao = 1 + 1;
+  console.log(adicao);
+
+  //subtração 
+  var subtracao = 2 - 1;
+  console.log(subtracao); 
+
+  //multiplicação 
+  var multiplicacao = 2 * 3; 
+  console.log(multiplicacao);
+
+  //divisão real 
+  var divisaoReal = 6 / 2; 
+  console.log(divisaoReal);  
+
+  //divisão inteira 
+  var divisaoInteria = 5 % 2; console);; 
+
+
+  //potenciação 
+  var potenciacao = 2**10; 
+  console.log(divisaoInteria);;
+
+
+  /* // operadores relacionais 
+  //maior que 
+  var maiorQue = 5 > 2; 
+  console.log(maiorQue); 
+
+  //menor que 
+  var menorQue = 5 < 2; 
+  console.log(menorQue); 
+
+  //maior ou igual a 
+  var maiorOuIgual = 5 >= 2; 
+  console.log (maiorOuIgual); 
+   
+  //menor ou igual a 
+  var menorOuIgual = 5 <= 2; 
+  console.log(menorOuIgual); */ 
+
+
+   /* // operadores lógicos 
+   // && - todos os valores devem ser true 
+   var e = true && true; 
+   console.log(e); 
+
+// || - somente um valor deve ser true 
+var ou = true || false; 
+console.log(ou); 
+
+// ! - inverte os valores 
+var nao = !true; 
+console.log(nao); */
